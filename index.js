3bf601b977b53002731b720184f5230c1f3d29f6
Nivethan <krowemoh@hotmail.com>
2022-02-12 17:20:09 -0500

    setting up package

--- index.js
+++ index.js
@@ -1,17 +1,8 @@
-const mv = require("./src/mv");
+const mvConverter = require("./src/mv-converter");
+const mvMatrix = require("./src/mv-matrix");
 
-let a = mv.MVMatrix();
-
-a[[1]] = 1;
-a[[2]] = 2;
-a[[2,1]] = 3
-a[[2,2]] = 4
-a[[2,3]] = 5
-a[[2,3,1]] = 6
-a[[2,3,2]] = 7
-a[[2,3,2,2]] = 8
-
-console.log("MV String: " + a.MVString);
-console.log("Test 1: ", a.MVString === "1þ3ý4ý6ü7û8");
-console.log("Test 2: ", mv.ArrayToMV(a.source) === "1þ3ý4ý6ü7û8");
-console.log("Value at (2,3,1): ", a[[2,3,1]]);
+module.exports = {
+    MVToArray: mvConverter.MVToArray, 
+    ArrayToMV: mvConverter.ArrayToMV,
+    MVMatrix: mvMatrix.MVMatrix,
+}
--- src/mv.js
+++
@@ -1,8 +0,0 @@
-const mvConverter = require("./mv-converter");
-const mvMatrix = require("./mv-matrix");
-
-module.exports = {
-    MVToArray: mvConverter.MVToArray, 
-    ArrayToMV: mvConverter.ArrayToMV,
-    MVMatrix: mvMatrix.MVMatrix,
-}
